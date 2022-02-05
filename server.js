b1ab26983575d2329868822a304226bc2547ec4f
Jake Finn <lof.steven@gmail.com>
2022-02-05 15:35:59 -0600

    Make the width of the message conatiner smaller

--- server.js
+++ server.js
@@ -12,10 +12,28 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+// });
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('chat message', (msg) => {
+    // console.log('message: ' + msg);
+  });
+});
+
+// io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
+
+// io.on('connection', (socket) => {
+//   socket.broadcast.emit('hi');
+// });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 });
 
